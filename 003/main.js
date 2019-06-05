14a6027265aef507c59a692a67ae6716676078db
unknown <katenkak551@example.com>
2019-06-05 19:01:51 +0300

    fifth comment

--- 003/main.js
+++ 003/main.js
@@ -54,137 +54,350 @@
 //2
 // Используйте уже написанные компоненты и добавьте в него поиск по имени.
 // Поиск должен работать моментально: по мере ввода текста в поле для поиска, должен фильтроваться список.
-const MEMBERS = [
-    {
-        id: 1,
-        name: 'Василий',
-        surname: 'Пупкин',
-        // author: 'Василий Пупкин',
-        avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
-        city: 'Киев'
-    },
-    {
-        id: 2,
-        name: 'Петр',
-        surname: 'Петров',
-        //author: 'Петр Петров',
-        avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
-        city: 'Львов'
-    },
-    {
-        id: 3,
-        name: 'Анастасия',
-        surname: 'Адвокатова',
-        //author: 'Анастасия Адвокатова',
-        avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
-        city: 'Сумы'
-    },
-    {
-        id: 4,
-        name: 'Анна',
-        surname: 'Морозова',
-        //author: 'Анна Морозова',
-        avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
-        city: 'Днепр'
-    },
+// const MEMBERS = [
+//     {
+//         id: 1,
+//         author: 'Василий Пупкин',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Киев'
+//     },
+//     {
+//         id: 2,
+//         author: 'Петр Петров',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Львов'
+//     },
+//     {
+//         id: 3,
+//         author: 'Анастасия Адвокатова',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Сумы'
+//     },
+//     {
+//         id: 4,
+//         author: 'Анна Морозова',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Днепр'
+//     },
+// ];
+//
+// const GroupList = React.createClass({
+//     render() {
+//         const {
+//             avatar,
+//             author,
+//             city
+//         } = this.props;
+//
+//         return (
+//             <div className="list">
+//                 <img className="list-avatar" src={avatar} />
+//
+//                 <div className="list-body">
+//                     <p>{author}</p>
+//
+//                     <i className="fas fa-map-marker-alt"/>
+//                     <span className="list-city">{city}</span>
+//
+//                     <div className="list-stats">
+//                         <div className="list-icons">
+//                             <a
+//                                 className="list-author"
+//                                 href={'https://facebook.com/'}
+//                                 target="blank"
+//                             >
+//                                 <i className="fab fa-facebook-square"/>
+//                             </a>
+//                         </div>
+//                         <div className="list-icons">
+//                             <a
+//                                 className="list-author"
+//                                 href={'https://github.com/'}
+//                                 target="blank"
+//                             >
+//                                 <i className="fab fa-github-square"/>
+//                             </a>
+//                         </div>
+//                         <div className="list-icons">
+//                             <a
+//                                 className="list-author"
+//                                 href={'https://mail.google.com/'}
+//                                 target="blank"
+//                             >
+//                                 <i className="fas fa-envelope-square"/>
+//                             </a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// });
+//
+// const Feed = React.createClass({
+//     getInitialState() {
+//         return {
+//             value: 'Поиск...'
+//         }
+//     },
+//
+//     searchName(event) {
+//       this.setState({value: event.target.value});
+//     },
+//
+//     render() {
+//         const lowercasedValue = this.state.value.toLowerCase();
+//         const filteredData = MEMBERS.filter(item => {
+//             return Object.keys(item).some(key =>
+//                 typeof item[key] === "string" && item[key].toLowerCase().includes(lowercasedValue)
+//             );
+//         });
+//         return (
+//             <div>
+//                 <h1 className="list-header">Meet React Course Members</h1>
+//                 <div className="searchWrap">
+//                     <i className="fas fa-search"/>
+//                     <input type="text" placeholder={this.state.value} className="searchInput" onChange={(e) => this.searchName(e)} id="inputs"/>
+//                 </div>
+//                 {
+//                     filteredData.map(member =>
+//                         <GroupList
+//                             key={member.id}
+//                             author={member.author}
+//                             city={member.city}
+//                             avatar={member.avatar}
+//                         />
+//                     )
+//                 }
+//             </div>
+//         );
+//     }
+// });
+//
+// ReactDOM.render(
+//     <Feed />,
+//     document.getElementById('root')
+// );
+
+//3
+const  NOTE = [{ id: 1, color: 'yellow', text: 'Hello, I\'m a first note!'}];
+const DEFAULT_COLOR = 'yellow';
+const COLORS = [
+    '#E63462',
+    '#FE5F55',
+    '#8bef54',
+    '#008536',
+    '#00B4A6',
+    '#007DB6',
+    '#9c00ff',
 ];
 
-const GroupList = React.createClass({
-    render() {
+const Note = React.createClass ({
+    handleDelete() {
+        this.props.onDelete(this.props.id);
+    },
+
+    render (){
         const {
-            avatar,
-            //author,
-            city,
-            name,
-            surname
+            color,
+            children
         } = this.props;
 
         return (
-            <div className="list">
-                <img className="list-avatar" src={avatar} />
-
-                <div className="list-body">
-                    <p>{name}</p>
-                    <p>{surname}</p>
-
-                    <i className="fas fa-map-marker-alt"/>
-                    <span className="list-city">{city}</span>
-
-                    <div className="list-stats">
-                        <div className="list-icons">
-                            <a
-                                className="list-author"
-                                href={'https://facebook.com/'}
-                                target="blank"
-                            >
-                                <i className="fab fa-facebook-square"/>
-                            </a>
-                        </div>
-                        <div className="list-icons">
-                            <a
-                                className="list-author"
-                                href={'https://github.com/'}
-                                target="blank"
-                            >
-                                <i className="fab fa-github-square"/>
-                            </a>
-                        </div>
-                        <div className="list-icons">
-                            <a
-                                className="list-author"
-                                href={'https://mail.google.com/'}
-                                target="blank"
-                            >
-                                <i className="fas fa-envelope-square"/>
-                            </a>
-                        </div>
-                    </div>
+            <div className="note" style={{backgroundColor: color}}>
+                <div>
+                    <span className="delete_note" onClick={this.handleDelete}> x </span>
                 </div>
+                { children }
             </div>
-        );
-    }
+        )
+    },
 });
 
-const Feed = React.createClass({
+const ColorPicker = React.createClass ({
     getInitialState() {
         return {
-            value: 'Поиск...'
+            image: ''
         }
     },
 
-    searchName(event) {
-      this.setState({value: event.target.value.toLowerCase()});
-      let matches = MEMBERS.filter((value) => {return value.name.indexOf(this.state.value) !== -1});
-      console.log(matches);
+    handlePutIcon() {
+        this.setState({
+            image: <i className="fas fa-check"/>
+        });
     },
 
     render() {
         return (
-            <div>
-                <h1 className="list-header">Meet React Course Members</h1>
-                <div className="searchWrap">
-                    <i className="fas fa-search"/>
-                    <input type="text" placeholder={this.state.value} className="searchInput" onChange={(e) => this.searchName(e)} id="inputs"/>
-                </div>
-                {
-                    MEMBERS.map(member =>
-                        <GroupList
-                            key={member.id}
-                            // author={member.author}
-                            name={member.name}
-                            surnname={member.surname}
-                            city={member.city}
-                            avatar={member.avatar}
-                        />
-                    )
-                }
+            <div className="allCircles">
+                <div className="colorCircle" style={{ backgroundColor: '#E63462'}} onClick={this.handlePutIcon} id="redColor">{this.state.image}</div>
+                <div className="colorCircle" style={{ backgroundColor: '#FE5F55'}}>{this.state.image}</div>
+                <div className="colorCircle" style={{ backgroundColor: '#8bef54'}}>{this.state.image}</div>
+                <div className="colorCircle" style={{ backgroundColor: '#008536'}}>{this.state.image}</div>
+                <div className="colorCircle" style={{ backgroundColor: '#00B4A6'}}>{this.state.image}</div>
+                <div className="colorCircle" style={{ backgroundColor: '#007DB6'}}>{this.state.image}</div>
+                <div className="colorCircle" style={{ backgroundColor: '#9c00ff'}}>{this.state.image}</div>
             </div>
-        );
+        )
     }
 });
 
-ReactDOM.render(
-    <Feed />,
+const NoteEditor = React.createClass ({
+    getInitialState() {
+        return {
+            text: ''
+        }
+    },
+
+    handleTextChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    },
+
+    handleNoteAdd() {
+        const newNote = {
+            text: this.state.text,
+            id: Date.now(),
+            color: DEFAULT_COLOR
+        };
+        this.props.onNoteAdd(newNote);
+
+        this.resetState();
+    },
+
+    resetState() {
+        this.setState({
+            text: '',
+        });
+    },
+
+    render (){
+        return (
+            <div className="editor">
+                <textarea
+                    rows={5}
+                    placeholder="Enter your note here..."
+                    className="editor_textarea"
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+
+                <ColorPicker />
+
+                {
+                    // this.state.text
+                    // &&
+                    <button
+                        className="editor_button"
+                        onClick={this.handleNoteAdd}
+                    >
+                        Add
+                    </button>
+                }
+            </div>
+        )
+    },
+});
+
+const NoteGrid = React.createClass ({
+    componentDidMount() {
+        this.msnry = new Masonry(this.grid, {
+            columnWidth: 300,
+            gutter: 10,
+            isFitWidth: true
+        })
+    },
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.notes !== this.props.notes){
+            this.msnry.reloadItems();
+            this.msnry.layout();
+        }
+    },
+
+    render (){
+        const {
+            notes,
+            onNoteDelete
+        } = this.props;
+
+        return (
+            <div className="grid" ref={c => this.grid = c}>
+                {
+                    notes.map(note =>
+                        <Note
+                            key={note.id}
+                            id={note.id}
+                            color={note.color}
+                            onDelete={onNoteDelete}
+                        >
+                            {note.text}
+                        </Note>
+                    )
+                }
+            </div>
+        )
+    },
+});
+
+const NotesApp = React.createClass ({
+    getInitialState() {
+        return {
+            notes: []
+        }
+    },
+
+    componentDidMount() {
+        const savedNotes = JSON.parse(localStorage.getItem('notes'));
+
+        if(savedNotes) {
+            this.setState({ notes: savedNotes})
+        }
+    },
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.notes !== this.state.notes) {
+            this.saveToLocalStorage();
+        }
+    },
+
+    handleNoteAdd(newNote) {
+        this.setState({
+            notes: [newNote, ...this.state.notes]
+        });
+    },
+
+    handleNoteDelete(noteId) {
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== noteId)
+        })
+    },
+
+    saveToLocalStorage() {
+        const notes = JSON.stringify(this.state.notes);
+
+        localStorage.setItem('notes', notes)
+    },
+
+    render (){
+        return (
+            <div style={{textAlign: 'center'}} className="app">
+                <h1 className="app_header">NoteApp</h1>
+
+                <NoteEditor onNoteAdd={this.handleNoteAdd}/>
+
+                <NoteGrid
+                    notes={this.state.notes}
+                    onNoteDelete={this.handleNoteDelete}
+                />
+            </div>
+        )
+    },
+});
+
+ReactDOM.render (
+    <NotesApp />,
     document.getElementById('root')
 );
 
