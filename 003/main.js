19ba7e699157a8f7814ef1149de62a29781eaf14
unknown <katenkak551@example.com>
2019-06-06 15:39:18 +0300

    color notes

--- 003/main.js
+++ 003/main.js
@@ -180,18 +180,188 @@
 // );
 
 //3
-const  NOTE = [{ id: 1, color: 'yellow', text: 'Hello, I\'m a first note!'}];
-const DEFAULT_COLOR = 'yellow';
-const COLORS = [
-    '#E63462',
-    '#FE5F55',
-    '#8bef54',
-    '#008536',
-    '#00B4A6',
-    '#007DB6',
-    '#9c00ff',
-];
+//1
+// Работать должно так. Вначале по центру экрана маленький круг (цвет на ваше усмотрение). При каждом клике на него, он увеличивается в размере. Так пока не достигнет максимального. Потом сначала.
+// Используйте только только inline-стили в этом задании.
+// const ViewCircle = React.createClass({
+//     getInitialState() {
+//         return {
+//             width: 20,
+//             height: 20,
+//             borderRadius: 10,
+//             marginTop: 471
+//         }
+//     },
+//
+//     changeCircleSize() {
+//         this.setState({
+//             width: this.state.width + 20,
+//             height: this.state.height + 20,
+//             borderRadius: this.state.borderRadius + 10,
+//             marginTop: this.state.marginTop - 10,
+//         });
+//         if (this.state.width > 900) {
+//             this.setState({
+//                 width: 20,
+//                 height: 20,
+//                 borderRadius: 10,
+//                 marginTop: 471
+//             })
+//         }
+//     },
+//
+//     render() {
+//         let drawCircle = {
+//             width: this.state.width,
+//             height: this.state.height,
+//             borderRadius: this.state.borderRadius,
+//             backgroundColor: 'black',
+//             marginTop: this.state.marginTop,
+//             marginRight: 'auto',
+//             marginLeft: 'auto'
+//         };
+//
+//         return (
+//             <div style={drawCircle} onClick={this.changeCircleSize}/>
+//         )
+//     }
+// });
+//
+// ReactDOM.render(
+//     <ViewCircle />,
+//     document.getElementById('root')
+// );
 
+
+//2
+// Используйте уже написанные компоненты и добавьте в него поиск по имени.
+// Поиск должен работать моментально: по мере ввода текста в поле для поиска, должен фильтроваться список.
+// const MEMBERS = [
+//     {
+//         id: 1,
+//         author: 'Василий Пупкин',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Киев'
+//     },
+//     {
+//         id: 2,
+//         author: 'Петр Петров',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Львов'
+//     },
+//     {
+//         id: 3,
+//         author: 'Анастасия Адвокатова',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Сумы'
+//     },
+//     {
+//         id: 4,
+//         author: 'Анна Морозова',
+//         avatar: 'https://advokat-spb.org/wp-content/uploads/2014/10/3dfc3f46ca8c3c8a1951307894b50dcf_images-for-profile-silhouette-unknown-person-clipart_1280-1024.jpeg',
+//         city: 'Днепр'
+//     },
+// ];
+//
+// const GroupList = React.createClass({
+//     render() {
+//         const {
+//             avatar,
+//             author,
+//             city
+//         } = this.props;
+//
+//         return (
+//             <div className="list">
+//                 <img className="list-avatar" src={avatar} />
+//
+//                 <div className="list-body">
+//                     <p>{author}</p>
+//
+//                     <i className="fas fa-map-marker-alt"/>
+//                     <span className="list-city">{city}</span>
+//
+//                     <div className="list-stats">
+//                         <div className="list-icons">
+//                             <a
+//                                 className="list-author"
+//                                 href={'https://facebook.com/'}
+//                                 target="blank"
+//                             >
+//                                 <i className="fab fa-facebook-square"/>
+//                             </a>
+//                         </div>
+//                         <div className="list-icons">
+//                             <a
+//                                 className="list-author"
+//                                 href={'https://github.com/'}
+//                                 target="blank"
+//                             >
+//                                 <i className="fab fa-github-square"/>
+//                             </a>
+//                         </div>
+//                         <div className="list-icons">
+//                             <a
+//                                 className="list-author"
+//                                 href={'https://mail.google.com/'}
+//                                 target="blank"
+//                             >
+//                                 <i className="fas fa-envelope-square"/>
+//                             </a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// });
+//
+// const Feed = React.createClass({
+//     getInitialState() {
+//         return {
+//             value: 'Поиск...'
+//         }
+//     },
+//
+//     searchName(event) {
+//       this.setState({value: event.target.value});
+//     },
+//
+//     render() {
+//         const lowercasedValue = this.state.value.toLowerCase();
+//         const filteredData = MEMBERS.filter(item => {
+//             return Object.keys(item).some(key =>
+//                 typeof item[key] === "string" && item[key].toLowerCase().includes(lowercasedValue)
+//             );
+//         });
+//         return (
+//             <div>
+//                 <h1 className="list-header">Meet React Course Members</h1>
+//                 <div className="searchWrap">
+//                     <i className="fas fa-search"/>
+//                     <input type="text" placeholder={this.state.value} className="searchInput" onChange={(e) => this.searchName(e)} id="inputs"/>
+//                 </div>
+//                 {
+//                     filteredData.map(member =>
+//                         <GroupList
+//                             key={member.id}
+//                             author={member.author}
+//                             city={member.city}
+//                             avatar={member.avatar}
+//                         />
+//                     )
+//                 }
+//             </div>
+//         );
+//     }
+// });
+//
+// ReactDOM.render(
+//     <Feed />,
+//     document.getElementById('root')
+// );
+
+//4
 const Note = React.createClass ({
     handleDelete() {
         this.props.onDelete(this.props.id);
@@ -214,38 +384,48 @@ const Note = React.createClass ({
     },
 });
 
-const ColorPicker = React.createClass ({
-    getInitialState() {
-        return {
-            image: ''
-        }
-    },
-
-    handlePutIcon() {
-        this.setState({
-            image: <i className="fas fa-check"/>
-        });
-    },
-
+const NoteSearch = React.createClass({
     render() {
         return (
-            <div className="allCircles">
-                <div className="colorCircle" style={{ backgroundColor: '#E63462'}} onClick={this.handlePutIcon} id="redColor">{this.state.image}</div>
-                <div className="colorCircle" style={{ backgroundColor: '#FE5F55'}}>{this.state.image}</div>
-                <div className="colorCircle" style={{ backgroundColor: '#8bef54'}}>{this.state.image}</div>
-                <div className="colorCircle" style={{ backgroundColor: '#008536'}}>{this.state.image}</div>
-                <div className="colorCircle" style={{ backgroundColor: '#00B4A6'}}>{this.state.image}</div>
-                <div className="colorCircle" style={{ backgroundColor: '#007DB6'}}>{this.state.image}</div>
-                <div className="colorCircle" style={{ backgroundColor: '#9c00ff'}}>{this.state.image}</div>
+            <input className="noteSearch" type="search" placeholder="Search..." onChange={this.props.onSearch}/>
+        );
+    }
+});
+
+const NoteColors = React.createClass({
+    render() {
+        let colors = ['#E63462', '#FE5F55', '#8bef54', '#008536', '#00B4A6', '#007DB6', '#9c00ff'];
+        return (
+            <div className="colors-list">
+                {
+                    colors.map((el, i) => {
+                        return (
+                            <div key={i} style={{backgroundColor: el}}>
+                                <input
+                                    className="radio-custom"
+                                    id={el}
+                                    type="radio"
+                                    name="color"
+                                    onChange={(e)=>this.props.onColorChanged(e, el)}
+                                />
+                                <label className="radio-custom-label" htmlFor={el}/>
+                            </div>
+                        );
+                    })
+                }
             </div>
-        )
+        );
     }
 });
 
 const NoteEditor = React.createClass ({
     getInitialState() {
+        this._hadleColorChange = this.hadleColorChange.bind(this);
+
         return {
-            text: ''
+            text: '',
+            color: '',
+            checked: false
         }
     },
 
@@ -259,11 +439,22 @@ const NoteEditor = React.createClass ({
         const newNote = {
             text: this.state.text,
             id: Date.now(),
-            color: DEFAULT_COLOR
+            color: this.state.color
         };
         this.props.onNoteAdd(newNote);
 
+        if(this.state.checked)
+            this.input.checked = false;
+
         this.resetState();
+    },
+
+    hadleColorChange(e, color) {
+        this.input = e.target;
+        this.setState({
+            color: color,
+            checked: e.target.checked
+        })
     },
 
     resetState() {
@@ -283,7 +474,9 @@ const NoteEditor = React.createClass ({
                     onChange={this.handleTextChange}
                 />
 
-                <ColorPicker />
+                <div className="controls">
+                    <NoteColors onColorChanged={this._hadleColorChange}/>
+                </div>
 
                 {
                     // this.state.text
@@ -400,4 +593,44 @@ ReactDOM.render (
     <NotesApp />,
     document.getElementById('root')
 );
+
+
+
+
+
+
+
+
+
+
+
+    // render() {
+    //     let searchQuery = this.state.value.toLowerCase();
+    //     let displayedNotes = !this.state.value ? this.props.notes : this.props.notes.filter(function(item) {
+    //         let searchValue = item.about.toLowerCase();
+    //         return searchValue.indexOf(searchQuery) !== -1;
+    //     });
+    //     console.log(displayedNotes);
+    //     return (
+    //         <div>
+    //             <NoteSearch onSearch={this.handleSearch}/>
+    //             <div ref={(div) => this.grid = div} className="notes-grid">
+    //                 {
+    //                     displayedNotes.map((note) => {
+    //                         return (
+    //                             <Note
+    //                                 key={note.id}
+    //                                 color={note.color}
+    //                                 onDelete={this.props.onNoteDelete.bind(null, note)}
+    //                             >{note.about}</Note>
+    //                         );
+    //                     })
+    //                 }
+    //             </div>
+    //         </div>
+    //     );
+    // }
+
+
+
 
